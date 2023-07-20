404850889ba9981ab92e530fe352a0ca3ba3e47a
Duš <62700665+G-rox-y@users.noreply.github.com>
2023-07-20 16:32:58 +0200

    Separate window into its own file

---
+++ src/window.hpp
@@ -0,0 +1,9 @@
+//----------------------------------------------------------
+
+#pragma once
+
+#include "Camera.hpp"
+
+//----------------------------------------------------------
+
+void WindowSetup(ObjectDatabase& database, Camera& cam);
